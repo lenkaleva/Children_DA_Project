c746b457a227db7183bad5174d14aea952f95460
Aneta <aneta.kantorova@gmail.com>
2025-11-26 14:59:08 +0100

    Cleaning Homepage

--- app.py
+++ app.py
@@ -16,37 +16,8 @@ if 'df' not in st.session_state:
 df = st.session_state.df
 
 
-country_list = st.session_state.df['COUNTRY_NAME'].unique().tolist()
-years_list = st.session_state.df['YEAR'].unique().tolist()
-
-# ------------------------------------------------------------
-# SIDEBAR MENU
-# ------------------------------------------------------------
-with st.sidebar:
-    st.sidebar.title("📚 Menu")
-    filters = {}
-    selected_country = st.selectbox('Select Country', options = ['All'] + country_list)
-    selected_sex = st.multiselect('Select Gender', options = ['Girls', 'Boys'], default = ['Girls', 'Boys'] )
-    selected_years = st.multiselect('Select years', options = years_list, default = years_list)
 
 
-    filters = {
-        'COUNTRY_NAME': None if selected_country == 'All' else [selected_country, 'Czech Republic'],
-        'YEAR': None if selected_years == years_list else selected_years,
-        'SEX': None if selected_sex == ['Girls', 'Boys'] else selected_sex,
-}
-    
-
-filtered = st.session_state.data.copy()
-for col, val in filters.items():
-    if val is None:
-        continue
-    if isinstance(val, (list, tuple, set)):
-        filtered = filtered[filtered[col].isin(val)]
-    else:
-        filtered = filtered[filtered[col] == val]
-
-st.dataframe(filtered[0:100])
 
 
 
